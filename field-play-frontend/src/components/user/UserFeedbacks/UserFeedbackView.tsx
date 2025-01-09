6982195f844fcddcf6c7115c76574da564d8843b
Rehneo <368557@niuitmo.ru>
2025-01-09 07:40:19 +0300

    feat(frontend): create user feedback view

--- field-play-frontend/src/components/user/UserFeedbacks/UserFeedbackView.tsx
+++ field-play-frontend/src/components/user/UserFeedbacks/UserFeedbackView.tsx
@@ -1,13 +1,52 @@
 import "./UserFeedbackView.css"
-import {useState} from "react";
+import React, {useState} from "react";
+import {keepPreviousData, useQuery} from "@tanstack/react-query";
+import Page from "../../../interfaces/Page.ts";
+import FeedbackReadDto from "../../../interfaces/feedback/FeedbackReadDto.ts";
+import feedbackService from "../../../services/FeedbackService.ts";
+import UserFeedbackBlockContainer from "./UserFeedbackBlockContainer.tsx";
+import {CircularProgress, Pagination} from "@mui/material";
 
 const UserFeedbackView = () => {
     const [pageIndex, setPageIndex] = useState<number>(1);
 
-    return <div>
 
+    const handlePageIndexChange = (_event: React.ChangeEvent<unknown>, value: number) => {
+        setPageIndex(value);
+    };
+
+
+    const {
+        data: page = {content: [], totalElements: 0},
+        isError: isLoadingError,
+        isFetching: isFetching,
+        isLoading: isLoading,
+    } = useGetFeedbacks(pageIndex);
+
+    return <div className="feedback-view-container">
+        {isLoadingError ? <span className="text-red-600">Произошла ошибка при загрузке отзывов</span> : ''}
+        {isLoading || isFetching ? <CircularProgress/> : ''}
+        <UserFeedbackBlockContainer feedbacks={page.content}/>
+        <Pagination className="pagination" count={Math.ceil(page.totalElements / 6)} page={pageIndex}
+                    onChange={handlePageIndexChange}/>
     </div>
 
 }
 
 export default UserFeedbackView;
+
+function useGetFeedbacks(
+    pageIndex: number) {
+    return useQuery<Page<FeedbackReadDto>>({
+        queryKey: [
+            'my-feedbacks',
+            pageIndex
+        ],
+        queryFn: async () => {
+            const response = await feedbackService.getAllMy(pageIndex - 1, 6);
+            return response.data;
+        },
+        placeholderData: keepPreviousData,
+        refetchOnWindowFocus: false
+    });
+}
--- field-play-frontend/src/pages/user/PersonalPage.tsx
+++ field-play-frontend/src/pages/user/PersonalPage.tsx
@@ -2,32 +2,14 @@ import "./PersonalPage.css"
 import UserHeader from "../../components/header/UserHeader/UserHeader.tsx";
 import UserDetails from "../../components/user/UserDetails.tsx";
 import {useAuth} from "../../context/UserAuth.tsx";
-import UserFeedbackBlockContainer from "../../components/user/UserFeedbacks/UserFeedbackBlockContainer.tsx";
-import {DateTime} from "luxon";
+import UserFeedbackView from "../../components/user/UserFeedbacks/UserFeedbackView.tsx";
 
 const PersonalPage = () => {
     const {user} = useAuth();
     return <div className="personal-page-container">
         <UserHeader/>
         <UserDetails user={user!}/>
-        <UserFeedbackBlockContainer feedbacks={[
-            {
-                id: 1,
-                user: user!,
-                fieldName: 'Маршал Арена',
-                fieldId: 2,
-                message: 'Отличное поле, понравилось',
-                rating: 5,
-                createdAt: DateTime.now()
-            }, {
-                id: 1,
-                user: user!,
-                fieldName: 'Маршал Арена',
-                fieldId: 2,
-                message: 'Отличное поле, понравилось',
-                rating: 5,
-                createdAt: DateTime.now()
-            }]}/>
+        <UserFeedbackView/>
     </div>
 }
 

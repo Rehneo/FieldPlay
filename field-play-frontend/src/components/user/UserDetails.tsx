a842e36a08e15e3f73af9469b15806a026d4d0d7
Rehneo <368557@niuitmo.ru>
2025-01-09 06:27:33 +0300

    feat(frontend): create user details component

--- field-play-frontend/src/components/user/UserDetails.tsx
+++ field-play-frontend/src/components/user/UserDetails.tsx
@@ -1,15 +1,29 @@
 import User from "../../interfaces/auth/User.ts";
 import React from "react";
 import "./UserDetails.css"
+import {Role} from "../../interfaces/auth/Role.ts";
 
 interface UserDetailsProps {
     user: User;
 }
 
-const UserDetails:  React.FC<UserDetailsProps>  = ({user}) => {
+const UserDetails: React.FC<UserDetailsProps> = ({user}) => {
 
     return <div className="user-details-container">
+        <label className="personal-label">Личный кабинет</label>
+        <hr className="personal-line"/>
+        <label className="role-label">{user.role == Role.USER ? "Пользователь" : "Админ"}</label>
+        <div className="user-info-container">
+            <div className="photo-container">
+                <label className="photo-label">Фото?</label>
+            </div>
+            <div className="info-container">
+                <label className="id-label">ID: {user.id}</label>
+                <hr className="personal-line"/>
+                <label className="full-name-label">{user.firstName} {user.lastName}</label>
+            </div>
 
+        </div>
     </div>
 }
 

fe793f22337eaa67fc4627e0f6abb665644bee9e
Rehneo <368557@niuitmo.ru>
2025-01-09 07:49:51 +0300

    feat(frontend): create session service

---
+++ field-play-frontend/src/services/SessionService.ts
@@ -0,0 +1,32 @@
+import apiService from "./ApiService.ts";
+import SessionReadDto from "../interfaces/session/SessionReadDto.ts";
+import Page from "../interfaces/Page.ts";
+import {AxiosResponse} from "axios";
+import SessionCreateDto from "../interfaces/session/SessionCreateDto.ts";
+import SearchRequest from "../interfaces/search/SearchRequest.ts";
+import {SortRequest} from "../interfaces/sort/SortRequest.ts";
+
+class SessionService {
+    getAllMy = async (page: number, size: number): Promise<AxiosResponse<Page<SessionReadDto>>> => {
+        return apiService.get<Page<SessionReadDto>>(`/sessions/?page=${page}&size=${size}`)
+    }
+
+    search = async (search: SearchRequest | null, page: number, size: number, sort: SortRequest | null): Promise<AxiosResponse<Page<SessionReadDto>>> => {
+        let sortRequest = "";
+        if (sort?.list) {
+            sortRequest += sort.list.map((item) => `&sort=${item.key},${item.asc ? 'asc' : 'desc'}`).join('&sort=');
+        }
+        return apiService.post<Page<SessionReadDto>>
+        (
+            `/sessions/search?page=${page}&size=${size}${sortRequest}`,
+            search
+        )
+    }
+
+    create = async (session: SessionCreateDto): Promise<AxiosResponse<SessionReadDto>> => {
+        return apiService.post<SessionReadDto>(`/sessions`, session);
+    }
+}
+
+const sessionService = new SessionService();
+export default sessionService;

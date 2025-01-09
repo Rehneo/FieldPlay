00f17cda7041c6dc363bd7132d8c66f527e066e8
Rehneo <368557@niuitmo.ru>
2025-01-09 23:28:58 +0300

    feat(frontend): create user collapse navigator

--- field-play-frontend/src/components/header/UserHeader/UserHeader.tsx
+++ field-play-frontend/src/components/header/UserHeader/UserHeader.tsx
@@ -1,31 +1,54 @@
 import "../Header.css"
-import {Avatar} from "@mui/material";
+import "./UserHeader.css"
+import {Avatar, Collapse, List, ListItem, ListItemButton, ListItemText} from "@mui/material";
 import cityIcon from '../../../assets/city.svg'
 import footballIcon from '../../../assets/football.svg'
 import {useNavigate} from "react-router-dom";
 import {useAuth} from "../../../context/UserAuth.tsx";
+import {useState} from "react";
 
 const UserHeader = () => {
     const navigate = useNavigate();
-    const {user} = useAuth();
+    const {user, logout} = useAuth();
+    const [open, setOpen] = useState(false);
 
-    return <header className="header">
-        <div className="user-container">
-            <Avatar onClick={() => navigate("/me")}
-                    className="avatar"
-                    alt="User"
-                    sx={{width: 50, height: 50}}/>
-            <span>{user?.balance} р.</span>
-        </div>
-        <div className="logo-container" onClick={() => navigate("/fields")}>
-            <img src={footballIcon} className="icon" alt="Football Icon"/>
-            <span>Field Play</span>
-        </div>
-        <div className="city-container">
-            <span>Санкт-Петербург</span>
-            <img src={cityIcon} className="icon" alt="City Icon"/>
-        </div>
-    </header>
+    return <div>
+        <header className="header">
+            <div className="user-container">
+                <Avatar onClick={() => setOpen(!open)}
+                        className="avatar"
+                        alt="User"
+                        sx={{width: 50, height: 50}}/>
+                <span>{user?.balance} р.</span>
+            </div>
+            <div className="logo-container" onClick={() => navigate("/fields")}>
+                <img src={footballIcon} className="icon" alt="Football Icon"/>
+                <span>Field Play</span>
+            </div>
+            <div className="city-container">
+                <span>Санкт-Петербург</span>
+                <img src={cityIcon} className="icon" alt="City Icon"/>
+            </div>
+        </header>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+            <List className="user-list">
+                <ListItem disablePadding>
+                    <ListItemButton onClick={() => {
+                        navigate("/me")
+                    }}>
+                        <ListItemText primary="Личный Кабинет"/>
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton onClick={() => {
+                        logout()
+                    }}>
+                        <ListItemText primary="Выйти"/>
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Collapse>
+    </div>
 }
 
 export default UserHeader;
